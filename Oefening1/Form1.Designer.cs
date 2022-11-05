3d390ead2eb7bb01e7d11cfc27889d34eedc1048
n4wf3l <nawfel.ajari@student.ehb.be>
2022-11-05 02:34:35 +0100

    1en2inOrde

--- Oefening1/Form1.Designer.cs
+++ Oefening1/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Oefening2
+﻿namespace Opdracht5Oefening1
 {
     partial class Form1
     {
@@ -29,31 +29,176 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redCtrlRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenCtrlGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueCtrlBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.smallCtrlSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.normalCtrlNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.largeCtrlLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.formatToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(3, 1, 0, 1);
-            this.menuStrip1.Size = new System.Drawing.Size(495, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "File";
-            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorToolStripMenuItem,
+            this.backgroundColorToolStripMenuItem,
+            this.fontToolStripMenuItem});
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.formatToolStripMenuItem.Text = "Format";
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.redCtrlRToolStripMenuItem,
+            this.greenCtrlGToolStripMenuItem,
+            this.blueCtrlBToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.colorToolStripMenuItem.Text = "Color";
+            // 
+            // redCtrlRToolStripMenuItem
+            // 
+            this.redCtrlRToolStripMenuItem.Name = "redCtrlRToolStripMenuItem";
+            this.redCtrlRToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.redCtrlRToolStripMenuItem.Text = "Red            Ctrl+A";
+            this.redCtrlRToolStripMenuItem.Click += new System.EventHandler(this.redCtrlRToolStripMenuItem_Click);
+            // 
+            // greenCtrlGToolStripMenuItem
+            // 
+            this.greenCtrlGToolStripMenuItem.Name = "greenCtrlGToolStripMenuItem";
+            this.greenCtrlGToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.greenCtrlGToolStripMenuItem.Text = "Green         Ctrl+G";
+            this.greenCtrlGToolStripMenuItem.Click += new System.EventHandler(this.greenCtrlGToolStripMenuItem_Click);
+            // 
+            // blueCtrlBToolStripMenuItem
+            // 
+            this.blueCtrlBToolStripMenuItem.Name = "blueCtrlBToolStripMenuItem";
+            this.blueCtrlBToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.blueCtrlBToolStripMenuItem.Text = "Blue             Ctrl+B";
+            this.blueCtrlBToolStripMenuItem.Click += new System.EventHandler(this.blueCtrlBToolStripMenuItem_Click);
+            // 
+            // backgroundColorToolStripMenuItem
+            // 
+            this.backgroundColorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.redToolStripMenuItem,
+            this.greenToolStripMenuItem,
+            this.blueToolStripMenuItem});
+            this.backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
+            this.backgroundColorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.backgroundColorToolStripMenuItem.Text = "Background Color";
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.redToolStripMenuItem.Text = "Red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redToolStripMenuItem_Click);
+            // 
+            // greenToolStripMenuItem
+            // 
+            this.greenToolStripMenuItem.Name = "greenToolStripMenuItem";
+            this.greenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.greenToolStripMenuItem.Text = "Green";
+            this.greenToolStripMenuItem.Click += new System.EventHandler(this.greenToolStripMenuItem_Click);
+            // 
+            // blueToolStripMenuItem
+            // 
+            this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
+            this.blueToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.blueToolStripMenuItem.Text = "Blue";
+            this.blueToolStripMenuItem.Click += new System.EventHandler(this.blueToolStripMenuItem_Click);
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.smallCtrlSToolStripMenuItem,
+            this.normalCtrlNToolStripMenuItem,
+            this.largeCtrlLToolStripMenuItem});
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fontToolStripMenuItem.Text = "Font";
+            // 
+            // smallCtrlSToolStripMenuItem
+            // 
+            this.smallCtrlSToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.smallCtrlSToolStripMenuItem.Name = "smallCtrlSToolStripMenuItem";
+            this.smallCtrlSToolStripMenuItem.Size = new System.Drawing.Size(250, 30);
+            this.smallCtrlSToolStripMenuItem.Text = "Small              Ctrl+S";
+            this.smallCtrlSToolStripMenuItem.Click += new System.EventHandler(this.smallCtrlSToolStripMenuItem_Click);
+            // 
+            // normalCtrlNToolStripMenuItem
+            // 
+            this.normalCtrlNToolStripMenuItem.Name = "normalCtrlNToolStripMenuItem";
+            this.normalCtrlNToolStripMenuItem.Size = new System.Drawing.Size(250, 30);
+            this.normalCtrlNToolStripMenuItem.Text = "Normal          Ctrl+N";
+            this.normalCtrlNToolStripMenuItem.Click += new System.EventHandler(this.normalCtrlNToolStripMenuItem_Click);
+            // 
+            // largeCtrlLToolStripMenuItem
+            // 
+            this.largeCtrlLToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.largeCtrlLToolStripMenuItem.Name = "largeCtrlLToolStripMenuItem";
+            this.largeCtrlLToolStripMenuItem.Size = new System.Drawing.Size(250, 30);
+            this.largeCtrlLToolStripMenuItem.Text = "Large              Ctrl+Z";
+            this.largeCtrlLToolStripMenuItem.Click += new System.EventHandler(this.largeCtrlLToolStripMenuItem_Click);
+            // 
+            // tbText
+            // 
+            this.tbText.Location = new System.Drawing.Point(0, 148);
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(800, 23);
+            this.tbText.TabIndex = 2;
+            this.tbText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbText_KeyDown);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(495, 265);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbText);
             this.Controls.Add(this.menuStrip1);
-            this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.Name = "Form1";
-            this.Text = "Parent Form";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -62,6 +207,22 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redCtrlRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greenCtrlGToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blueCtrlBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backgroundColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem smallCtrlSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem normalCtrlNToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem largeCtrlLToolStripMenuItem;
+        private System.Windows.Forms.TextBox tbText;
     }
 }
 
--- Oefening1/Form1.cs
+++ Oefening1/Form1.cs
@@ -1,3 +1,4 @@
+﻿using Microsoft.VisualBasic.Devices;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,29 +9,92 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Oefening2
+namespace Opdracht5Oefening1
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            tbText.BorderStyle = BorderStyle.None;
+            tbText.Cursor = Cursors.Default;
+            /*tbText.ReadOnly = true;*/
+            tbText.TabStop = false;
+            tbText.Text = "How sharper than a serpent's tooth it is to have a buggy program";
         }
 
-        private void Form1_Show(object sender, EventArgs e)
+        private void tbText_KeyDown(object sender, KeyEventArgs e)
         {
-            Form2 tweedeForm = new Form2();
-            tweedeForm.Show();
+            if (e.KeyCode == Keys.E && e.Modifiers == Keys.Control)
+            {
+                tbText.ForeColor = System.Drawing.Color.Red;
+            }else if(e.KeyCode == Keys.G && e.Modifiers == Keys.Control)
+            {
+                tbText.ForeColor = System.Drawing.Color.Green;
+            }else if(e.KeyCode == Keys.B && e.Modifiers == Keys.Control)
+            {
+                tbText.ForeColor = System.Drawing.Color.Blue;
+            }else if(e.KeyCode == Keys.S && e.Modifiers == Keys.Control)
+            {
+                tbText.Font = new Font("Segoe UI", 7);
+            }else if(e.KeyCode == Keys.N && e.Modifiers == Keys.Control)
+            {
+                tbText.Font = new Font("Segoe UI", 9);
+            }
+            else if (e.KeyCode == Keys.Z && e.Modifiers == Keys.Control)
+            {
+                tbText.Font = new Font("Segoe UI", 14);
+            }
         }
-
-        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+           this.Close();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void redCtrlRToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            tbText.ForeColor = System.Drawing.Color.Red;
         }
+
+        private void greenCtrlGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbText.ForeColor = System.Drawing.Color.Green;
+        }
+
+        private void blueCtrlBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbText.ForeColor = System.Drawing.Color.Blue;
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbText.BackColor = System.Drawing.Color.Red;
+        }
+
+        private void greenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbText.BackColor = System.Drawing.Color.Green;
+        }
+
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbText.BackColor = System.Drawing.Color.Blue;
+        }
+
+        private void largeCtrlLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbText.Font = new Font("Segoe UI", 14);
+        }
+
+        private void normalCtrlNToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbText.Font = new Font("Segoe UI", 9);
+        }
+        private void smallCtrlSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbText.Font = new Font("Segoe UI", 7);
+        }
+
     }
 }
